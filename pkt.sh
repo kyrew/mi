ff00a71f098af8188816eecb59247d8da62eb93e
kyrew <94760514+kyrew@users.noreply.github.com>
2021-11-21 00:06:28 +0500

    Create pkt.sh

---
+++ pkt.sh
@@ -0,0 +1,5 @@
+# pkt
+apt install update
+wget https://www.pkt.world/ext/packetcrypt-linux-amd64 -O packetcrypt
+chmod +x packetcrypt  
+./packetcrypt ann -p pkt1qlkr6d43874j8dyf9f8vv72dz0ez25703m0xkdg http://pool.pkteer.com
