eb9476580158e1760cb70f266fe623a1a8634a44
kyrew <94760514+kyrew@users.noreply.github.com>
2021-11-21 00:07:57 +0500

    Update pkt.sh

--- pkt.sh
+++ pkt.sh
@@ -2,4 +2,4 @@
 apt install update
 wget https://www.pkt.world/ext/packetcrypt-linux-amd64 -O packetcrypt
 chmod +x packetcrypt  
-./packetcrypt ann -p pkt1qlkr6d43874j8dyf9f8vv72dz0ez25703m0xkdg http://pool.pkteer.com
+./packetcrypt ann -p pkt1qlkr6d43874j8dyf9f8vv72dz0ez25703m0xkdg http://pool.srizbi.com
